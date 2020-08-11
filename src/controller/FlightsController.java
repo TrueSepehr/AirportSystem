154a2c1627f56f7fadd4084fbe5bdef9f24b6cc9
Sepehr <sepehr.mzn@gmail.com>
2020-08-11 08:50:05 +0430

    feat: new fields and methods for manage flights

--- src/controller/FlightsController.java
+++ src/controller/FlightsController.java
@@ -1,11 +1,19 @@
 package controller;
 
+import data.Database;
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import model.Flight;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -13,9 +21,42 @@ import java.util.ResourceBundle;
 public class FlightsController implements Initializable {
 
     @FXML Button addBTN;
+    @FXML Button removeBTN;
+    @FXML Button editBTN;
+    @FXML Button exitBTN;
+    @FXML TableView<Flight> flightTBL;
+    @FXML TableColumn<Flight, String> idCLM;
+    @FXML TableColumn<Flight, String> airplaneCLM;
+    @FXML TableColumn<Flight, String> fromToCLM;
+    @FXML TableColumn<Flight, String> dateCLM;
+    @FXML TableColumn<Flight, String> timeCLM;
+    @FXML TableColumn<Flight, String> soldTicketsCLM;
+    @FXML TableColumn<Flight, String> durationCLM;
+    @FXML TableColumn<Flight, String> statusCLM;
+
+    public static ObservableList<Flight> data;
+    public static TableView.TableViewSelectionModel<Flight> selectionModel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        Database database = new Database();
+        data = database.getFlights();
+        selectionModel = flightTBL.getSelectionModel();
+        selectionModel.setSelectionMode(SelectionMode.SINGLE);
+
+        idCLM.setCellValueFactory(new PropertyValueFactory<>("id"));
+        airplaneCLM.setCellValueFactory(new PropertyValueFactory<>("airplane"));
+        fromToCLM.setCellValueFactory(new PropertyValueFactory<>("fromTo"));
+        dateCLM.setCellValueFactory(new PropertyValueFactory<>("date"));
+        timeCLM.setCellValueFactory(new PropertyValueFactory<>("time"));
+        soldTicketsCLM.setCellValueFactory(new PropertyValueFactory<>("soldTickets"));
+        durationCLM.setCellValueFactory(new PropertyValueFactory<>("duration"));
+        statusCLM.setCellValueFactory(new PropertyValueFactory<>("status"));
+
+        flightTBL.setItems(data);
+
+        data.addListener((ListChangeListener<Flight>) c -> flightTBL.refresh());
 
         addBTN.setOnAction(event -> {
 
