508dd3533b545f56702cfa548db5c312379e6a46
Sepehr <sepehr.mzn@gmail.com>
2020-08-11 08:48:12 +0430

    feat: new static observable list for refresh table view

--- src/controller/AirplaneController.java
+++ src/controller/AirplaneController.java
@@ -1,11 +1,11 @@
 package controller;
 
+import javafx.collections.ListChangeListener;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import model.Airplane;
 import data.Database;
 import java.net.URL;
@@ -20,16 +20,43 @@ public class AirplaneController implements Initializable {
     @FXML TableColumn<Airplane, String> airplaneIdCLM;
     @FXML TableColumn<Airplane, String> seatsCLM;
 
+    public static ObservableList<Airplane> data;
+    public static TableView.TableViewSelectionModel<Airplane> selectionModel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+
+        Database database = new Database();
+        data = database.getAirplanes();
+        selectionModel = airplaneTBL.getSelectionModel();
+        selectionModel.setSelectionMode(SelectionMode.SINGLE);
+
+        airplaneIdCLM.setCellValueFactory(new PropertyValueFactory<>("id"));
+        seatsCLM.setCellValueFactory(new PropertyValueFactory<>("seats"));
+
+        airplaneTBL.setItems(data);
+
+        data.addListener((ListChangeListener<Airplane>) c -> airplaneTBL.refresh());
 
         addBTN.setOnAction(event -> {
 
             Airplane airplane = new Airplane(airplaneIdFLD.getText(), seatsFLD.getText());
-            Database database = new Database();
             database.addAirplane(airplane);
+            data.add(airplane);
 
         });
 
     }
+
+    public Airplane gatAirplaneById(String id) {
+
+        for (Airplane datum : data) {
+            if (datum.getId().equalsIgnoreCase(id)) {
+                return datum;
+            }
+        }
+
+        return null;
+    }
+
 }
