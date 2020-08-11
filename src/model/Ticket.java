65913ca426b80910772a6da9f3fb56ee4d647f1c
Sepehr <sepehr.mzn@gmail.com>
2020-08-11 08:42:40 +0430

    feat: new constructor, getter and setter (ticket model)

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -6,8 +6,43 @@ public class Ticket implements Showable {
     private String price;
     private String penaltyRate;
 
+    public Ticket(String id, String price, String penaltyRate) {
+        this.id = id;
+        this.price = price;
+        this.penaltyRate = penaltyRate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getPenaltyRate() {
+        return penaltyRate;
+    }
+
+    public void setPenaltyRate(String penaltyRate) {
+        this.penaltyRate = penaltyRate;
+    }
+
     @Override
     public void show() {
 
+    }
+
+    @Override
+    public String toString() {
+        return getId();
     }
 }
